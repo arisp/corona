14c951c985bcb0d56eb79118c8725592cb5cf584
Aris Petridis <apetridis@factset.com>
2020-05-24 17:14:19 +0100

    Bypass api gateway and go through the alb

--- src/fetchData.js
+++ src/fetchData.js
@@ -5,12 +5,12 @@ export async function getTimelineData() {
     let requestPromises = [];
 
     for (let country of countryDataMap.keys()) {
-        let requestPromise = fetch('https://ypw2sf9hei.execute-api.us-east-1.amazonaws.com/prod/v2/historical/' + country + '?lastdays=60').then(async (response) => countryDataMap.get(country).data = await response.json());
+        let requestPromise = fetch('https://api.covid-stats.io/v2/historical/' + country + '?lastdays=60').then(async (response) => countryDataMap.get(country).data = await response.json());
         requestPromises.push(requestPromise);
     }
 
     for (let country of countryDataMap.keys()) {
-        let requestPromise = fetch('https://ypw2sf9hei.execute-api.us-east-1.amazonaws.com/prod/v2/countries/' + country).then(async (response) => countryDataMap.get(country).latest = await response.json());
+        let requestPromise = fetch('https://api.covid-stats.io/v2/countries/' + country).then(async (response) => countryDataMap.get(country).latest = await response.json());
         requestPromises.push(requestPromise);
     }
 
